18a1b37e133cc3358c0630b6adb31b42323d63b7
Pam18 <yha.lima@outlook.com>
2022-05-15 21:32:16 -0300

    feat: desenvolvimento do projeto de um mini comércio

--- src/main/java/br/com/mini_comercio/MiniComercioApplication.java
+++ src/main/java/br/com/mini_comercio/MiniComercioApplication.java
@@ -1,6 +1,5 @@
 package br.com.mini_comercio;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.mini_comercio.controller.MenuComercio;
